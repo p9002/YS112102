5f96e6631eff72fc0c09dfd8336dabf59d0752aa
alan <p9002>
2016-11-21 14:19:06 +0800

    抓取udn.com網頁

---
+++ app/src/main/java/alan/no1/ys112102/MyDataHandler.java
@@ -0,0 +1,19 @@
+package alan.no1.ys112102;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by user on 2016/11/21.
+ */
+
+public class MyDataHandler extends DefaultHandler {
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        Log.d("XML", qName);
+    }
+}
