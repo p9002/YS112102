4c2724a53b16eae2dcafb951c6d85de9309e16a0
alan <p9002>
2016-11-21 14:28:44 +0800

    抓取udn.com網頁標題

--- app/src/main/java/alan/no1/ys112102/MyDataHandler.java
+++ app/src/main/java/alan/no1/ys112102/MyDataHandler.java
@@ -11,9 +11,32 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 
 public class MyDataHandler extends DefaultHandler {
+    boolean isTitle = false;
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        Log.d("XML", qName);
+        if (qName.equals("title"))
+        {
+            isTitle = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        if (qName.equals("title"))
+        {
+            isTitle = false;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+        if (isTitle)
+        {
+            String str = new String(ch).substring(start, start + length);
+            Log.d("XML", str);
+        }
     }
 }
